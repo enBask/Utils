e4182652cb3ced919b4d2f93aab2e9a995ee719e
Bill Hilke <whilke@gmail.com>
2015-03-15 06:51:03 -0700

    init.

---
+++ FastObjectAllocator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enbask.utils
+{    
+    public static class FastObjectAllocator<T> where T : new()
+    {
+        //since this is a static generic class, every type of T will have its
+        //own instance. This leads to a unique mObjectCreator for every type of T.
+        private static Func<T> mObjectCreator = null;
+
+        public static T New()
+        {
+            //lazy create the alloc generator
+            //This introduces a branch test for every allocation, which could be removed
+            //if you implemented a pre-create system for Types that use this.
+            if (mObjectCreator == null)
+            {
+                var objectType = typeof(T);
+                var defaultCtor = objectType.GetConstructor(new Type[] { });
+
+                var dynMethod = new DynamicMethod(
+                    name: string.Format("_{0:N}", Guid.NewGuid()),
+                    returnType: objectType,
+                    parameterTypes: null
+                    );
+
+                ILGenerator il = dynMethod.GetILGenerator();
+                il.Emit(OpCodes.Newobj, defaultCtor);
+                il.Emit(OpCodes.Ret);
+
+                mObjectCreator = dynMethod.CreateDelegate(typeof(Func<T>)) as Func<T>;
+            }
+
+            //There is no getting around having to make a function call to execute the newobj op code
+            //The the JIT will also never inline this, as it's a delegate call.
+            //This is why we can never be as fast as new Object() if the type is known at compile time.
+            return mObjectCreator();
+        }
+
+    }
+}
---
+++ Memoization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enbask
+{
+    public static partial class Utils
+    {
+        //Since this is a static generic, a unique static version will be created for every combo of Arg & Ret.
+        public static Func<Arg, Ret> Memoize<Arg, Ret>(this Func<Arg, Ret> functor)
+        {
+            //pay attention to what is being returned here. We are returning a lambda with the correct
+            //Func<Arg,Ret> signature.
+            //Along with that we are creating a dictionary instance outside the function, and using it within
+            //the anonymous method.
+
+            //since this is a static function for generics, it means every combo of Arg & Ret will have it's own
+            //dictionary instance.
+            var memo_table = new Dictionary<Arg, Ret>();
+            return arg0 =>
+            {
+                //very simple wrapper that checks if we've stored the Arg value -> return value lookup.
+                Ret func_return_val;
+                if (!memo_table.TryGetValue(arg0, out func_return_val))
+                {
+                    //this will be the expensive call as we have to call the function. The arg->ret set will
+                    //get added to the lookup for future calls.
+                    func_return_val = functor(arg0);
+                    memo_table.Add(arg0, func_return_val);
+                }
+                return func_return_val;
+            };
+        }
+    }
+
+
+}
